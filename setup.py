d3fa317532f0b2c4e2d9f52a60cdde4e8e1be961
kajob-dev <xd@xd.com>
2024-04-10 03:48:33 +0400

    basic setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+
+from setuptools import setup
+
+setup(
+    packages=[
+        os.path.join(root).replace("\\", ".") for root, _, files in os.walk("quack") if "__init__.py" in files
+    ]
+)
