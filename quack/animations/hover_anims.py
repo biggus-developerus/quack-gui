8e14bfbc712603a884d9747b09abd3533e04a5fd
biggus-developerus <xd@xd.com>
2024-05-08 17:15:45 +0400

    make anim_duration ACTUALLY time (second) based sob x69420)

--- quack/animations/hover_anims.py
+++ quack/animations/hover_anims.py
@@ -13,19 +13,18 @@ async def hover_dim(ctx: EventContext) -> None:
     colour = ctx.element.original_colour
 
     if ctx.element.animation_duration:
-        sleep_time = ctx.element.animation_duration / (ctx.app.get_fps() if ctx.app.get_fps() > 0 else 60)
-
         max_difference = max(abs(colour[i] - dimmed[i]) for i in range(3))
-
         step_sizes = [(dimmed[i] - colour[i]) / max_difference for i in range(3)]
 
-        for i in range(max_difference):
-            r = max(0, min(255, int(colour[0] + i * step_sizes[0])))
-            g = max(0, min(255, int(colour[1] + i * step_sizes[1])))
-            b = max(0, min(255, int(colour[2] + i * step_sizes[2])))
+        if max_difference > 0:
+            sleep_time = ctx.element.animation_duration / max_difference
+            for i in range(max_difference):
+                r = max(0, min(255, int(colour[0] + i * step_sizes[0])))
+                g = max(0, min(255, int(colour[1] + i * step_sizes[1])))
+                b = max(0, min(255, int(colour[2] + i * step_sizes[2])))
 
-            ctx.element.colour = (r, g, b)
-            await asyncio.sleep(sleep_time)
+                ctx.element.colour = (r, g, b)
+                await asyncio.sleep(sleep_time)
     else:
         ctx.element.colour = dimmed
 
@@ -35,12 +34,11 @@ async def unhover_dim(ctx: EventContext) -> None:
     colour = ctx.element.colour
 
     if ctx.element.animation_duration:
-        sleep_time = ctx.element.animation_duration / 60
-
         max_difference = max(abs(colour[i] - original_colour[i]) for i in range(3))
 
-        if max_difference != 0:  # prevent div by zero
+        if max_difference > 0: 
             step_sizes = [(original_colour[i] - colour[i]) / max_difference for i in range(3)]
+            sleep_time = ctx.element.animation_duration / max_difference
 
             for i in range(max_difference):
                 r = max(0, min(255, int(colour[0] + i * step_sizes[0])))
