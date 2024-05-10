da0d24fc64f990fbafce957587e92764b3f138c2
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-05-10 15:11:46 +0400

    remove element manager

--- quack/__init__.py
+++ quack/__init__.py
@@ -2,7 +2,6 @@ from .animation import *
 from .app import *
 from .dispatcher import *
 from .element import *
-from .element_manager import *
 from .elements import *
 from .font import *
 from .net import *
